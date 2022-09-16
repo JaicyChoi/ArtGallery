5f1d15312fcb11e02c3356ef0c00eca321348c8d
jaicychoi <jaicychoi@gamil.com>
2022-09-16 22:39:11 +0900

    subpage test

---
+++ js/data.js
@@ -0,0 +1,30 @@
+let DATA = {
+    INVEN: [
+        { file: '20220907', date: '2022.09.07', nickname: '꼬꼬빵' },
+        { file: '20220531', date: '2022.05.31', nickname: '김두유' },
+        { file: '20220330', date: '2022.03.30', nickname: 'nong' },
+        { file: '20220124', date: '2022.01.24', nickname: '꼬마도도' },
+        { file: '20211114', date: '2021.11.14', nickname: '참신한닉' },
+        { file: '20211113', date: '2021.11.13', nickname: '참신한닉' },
+        { file: '20211007', date: '2021.10.07', nickname: '웃기조아' },
+        { file: '20211006', date: '2021.10.06', nickname: 'nong' },
+        { file: '20210930', date: '2021.09.30', nickname: '참신한닉' },
+        { file: '20210812', date: '2021.08.12', nickname: '벤인' },
+        { file: '20210626', date: '2021.06.26', nickname: '꼬마도도' },
+        { file: '20210624', date: '2021.06.24', nickname: '꼬마도도' },
+        { file: '20210617', date: '2021.06.17', nickname: '꼬마도도' },
+        { file: '20210508', date: '2021.05.08', nickname: '꼬마도도' },
+        { file: '20210506', date: '2021.05.06', nickname: '참신한닉' },
+        { file: '20210112', date: '2021.01.12', nickname: '투세' },
+        { file: '20201009', date: '2020.10.09', nickname: '꼬마도도' },
+        { file: '20200928', date: '2020.09.28', nickname: '꼬마도도' },
+        { file: '20200617', date: '2020.06.17', nickname: '꼬마도도' },
+        { file: '20190823', date: '2019.08.23', nickname: '카벙클쿤' },
+    ],
+    SNS: [
+
+    ],
+    InGame: [
+
+    ]
+}
--- js/inven.js
+++ js/inven.js
@@ -1,3 +1,46 @@
 const menu_list_li_a = document.querySelectorAll('.menu_list>li>a');
+const wrapper = document.querySelector('.wrapper');
+
+for( let i = 0 ; i < DATA.INVEN.length ; i++ ){
+    let li = document.createElement('li');
+    let img_wrppaer = document.createElement('div');
+    let img = document.createElement('img');
+    let hover_info_wrapper = document.createElement('div');
+    let hover_info = document.createElement('div');
+    let hover_info_date = document.createElement('p');
+    let hover_info_nickname = document.createElement('p');
+
+    img_wrppaer.classList.add('img_wrppaer');
+    hover_info_wrapper.classList.add('hover_info_wrapper');
+    hover_info.classList.add('hover_info');
+    hover_info_date.classList.add('hover_info_date');
+    hover_info_nickname.classList.add('hover_info_nickname');
+
+    img.src = 'img/inven/' + DATA.INVEN[i].file + '.jpg';
+    img.alt = 'inven ' + DATA.INVEN[i].nickname + '님 그림';
+    hover_info_date.innerHTML = DATA.INVEN[i].date;
+    hover_info_nickname.innerHTML = DATA.INVEN[i].nickname + '@Inven';
+
+    hover_info.appendChild(hover_info_date);
+    hover_info.appendChild(hover_info_nickname);
+    hover_info_wrapper.appendChild(hover_info);
+    img_wrppaer.appendChild(img);
+    img_wrppaer.appendChild(hover_info_wrapper);
+    li.appendChild(img_wrppaer);
+    wrapper.append(li);
+}
+
+const wrapper_li = document.querySelectorAll('.wrapper>li');
+const hover_info_wrapper = document.querySelectorAll('.hover_info_wrapper');
+const wrapper_img = document.querySelectorAll('.img_wrppaer>img');
 
 menu_list_li_a[1].classList.add('menu_selected');
+wrapper_li.forEach( (li, index) => li.addEventListener('mouseenter', () => {
+    hover_info_wrapper[index].classList.add('hover');
+    wrapper_img[index].classList.add('hover');
+}));
+wrapper_li.forEach( (li, index) => li.addEventListener('mouseleave', () => {
+    hover_info_wrapper[index].classList.remove('hover');
+    wrapper_img[index].classList.remove('hover');
+}));
+
